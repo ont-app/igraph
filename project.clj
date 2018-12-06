142eb325d7ff6c77ae7dce9bea5c991f95c32584
Eric <eric.scott@acm.org>
2018-12-05 16:13:09 -0800

    Edited project.clj to support deployment to clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject igraph "0.1.0-SNAPSHOT"
+(defproject ont-app/igraph "0.1.0-SNAPSHOT"
   :description "Defines a IGraph protocol for maintaining a querying different graph implementations"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/ont-app/igraph"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
