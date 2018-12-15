592a3157cb7c63342df89c187d64612c0db76d46
Eric <eric.scott@acm.org>
2018-12-14 18:27:49 -0800

    Moved the traversal test out of core_test

--- test/igraph/core_test.clj
+++ test/igraph/core_test.clj
@@ -2,15 +2,3 @@
   (:require [clojure.test :refer :all]
             [igraph.core :refer :all]))
 
-(deftest traverse-test
-  (testing "Test traverse and transitive-closure"
-    (let [g (add (make-graph) [[:a :isa :b] [:b :isa :c][:c :isa :d]])
-          isa* (fn [g acc to-visit]
-                 [(conj acc (first to-visit))
-                  (concat to-visit (g (first to-visit) :isa))])
-          ]
-      (is (= (traverse g isa* [] [:a])
-             [:a :b :c :d]))
-      ;; transitive-closure writes equivalent of isa*...
-      (is (= (traverse g (transitive-closure :isa) [] [:a])
-             [:a :b :c :d])))))
