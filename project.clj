161a44596b56f132f37e6979eba613db6ec94971
Eric Scott <eric.scott@acm.org>
2021-08-20 09:08:41 -0700

    Removing SNAPSHOT in project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ont-app/igraph "0.1.6-SNAPSHOT"
+(defproject ont-app/igraph "0.1.6"
   :description "Defines a IGraph protocol for maintaining a querying different graph implementations"
   :url "https://github.com/ont-app/igraph"
   :license {:name "Eclipse Public License"
